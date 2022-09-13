3d0f7fa9faf3f7a4f4c775bce4a305a469375fa1
Serdar Yegulalp <serdar@genjipress.com>
2022-09-13 11:24:13 -0400

    clean up compile instructions

--- compile.py
+++ compile.py
@@ -20,12 +20,15 @@ ext_modules = [
         "life",
         ["life.py"],
         # Use this line only if you're compiling with MSVC.
-        extra_compile_args = ['/arch:AVX512', '/O2']
+        # extra_compile_args=["/arch:AVX512", "/O2"]
+        extra_compile_args=["/O2"]
+        # arch:AVX2
+        # arch:AVX512
         # Omit /arch:AVX512 line if the module crashes.
         # Use /MD for multithread DLL (not implemented yet)
     )
 ]
 
-os.chdir('src')
+os.chdir("src")
 
 setup(name="life", ext_modules=cythonize(ext_modules, annotate=True))
